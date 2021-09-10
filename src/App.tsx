69b64c2e21d0ed8771c88dfd6b5d9433f55cd8f1
Michael Goff <michaelgoff@Michaels-MacBook-Pro.local>
2021-09-10 00:36:11 -0700

    Better use of React structures

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,55 @@
-import React, { Component, MouseEvent, useState } from 'react'
+import React, { PureComponent, Component, MouseEvent, useState } from 'react'
 import './App.css'
+
+interface ResourceName {
+  resource_name: string,
+  auto_name: string
+}
+
+let GameDatabase: ResourceName[] = [
+  {resource_name: "Wood", auto_name:"Chop Wood"},
+  {resource_name: "Stone", auto_name:"Quarry Stone"},
+  {resource_name: "Copper", auto_name:"Mine Copper"},
+  {resource_name: "Iron", auto_name:"Mine Iron"}
+]
 
 interface Resource{
   resource_name: string,
   quantity: number
 }
 
-type TypeExample = {
-  resource_name: String,
-  quantity: number
-}
-
 let gameData: {[resource: string]: Resource[]} = {
-  "resources": [
-    {resource_name: "Wood", quantity:0},
-    {resource_name: "Stone", quantity:0},
-    {resource_name: "Copper", quantity:0},
-    {resource_name: "Iron", quantity:0}
-  ]
+  "resources": []
 };
-
-// This component is now superseded by TSResourceDisplay
-let ResourceDisplay = (resource: any) => {
-  return (
-    <p>
-      {resource.resource.resource_name} {resource.resource.quantity}
-    </p>
-  )
+for (let i=0; i<GameDatabase.length; i++) {
+  gameData.resources = gameData.resources.concat({resource_name:GameDatabase[i].resource_name,quantity:0});
 }
 
 interface ButtonParams {
   message: string,
   resource: Resource,
-  frameCount: any,
-  setFrameCount: any
+  setQuantity: any
 }
 
 export class Button extends Component <ButtonParams> {
-  handleClick(event: MouseEvent) {
-    event.preventDefault();
-    //alert(event.currentTarget.tagName); // alerts BUTTON
-  }
-  
   render() {
     return <button onClick={(event: MouseEvent)=>{
-      //this.handleClick(event);
       event.preventDefault();
       this.props.resource.quantity += 1;
-      this.props.setFrameCount(this.props.frameCount+1);
+      this.props.setQuantity(this.props.resource.quantity);
     }}>
       {this.props.message}
     </button>
   }
 }
 
-let TSResourceDisplay = ({ resource, frameCount, setFrameCount }: { resource: Resource, frameCount:any, setFrameCount:any }) => {
+let TSResourceDisplay = ({ resource }: { resource: Resource }) => {
+  const [quantity, setQuantity] = useState(resource.quantity);
+  
   return (
     <p>
-      {resource.resource_name} {resource.quantity}
-      <Button message="Harvest" resource={resource} frameCount={frameCount} setFrameCount={setFrameCount}/>
+      {resource.resource_name} {quantity}
+      <Button message="Harvest" resource={resource} setQuantity={setQuantity}/>
     </p>
   )
 }
@@ -70,7 +61,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         {gameData.resources.map(r => {
-          return <TSResourceDisplay resource={r} frameCount={frameCount} setFrameCount={setFrameCount} key={r["resource_name"]} />
+          return <TSResourceDisplay resource={r} key={r["resource_name"]} />
         }
           
         )}
